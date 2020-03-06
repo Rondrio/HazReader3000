bab5ee0967f2684bf57aa441bb4d551e507f10fd
Alexander Ganz <alexander.ganz@4com.de>
2020-03-06 18:25:56 +0100

    removed unnecessary debug messages

--- main.go
+++ main.go
@@ -27,14 +27,10 @@ func main() {
 	}
 
 	b := new(bytes.Buffer)
-	n, err := b.ReadFrom(resp.Body)
+	_, err = b.ReadFrom(resp.Body)
 	if err != nil {
 		log.Panic(err)
 		return
-	}
-
-	if n != resp.ContentLength {
-		log.Printf("Read %v, Bodylength : %v", n, resp.ContentLength)
 	}
 
 	file, err := os.Create("artikel.md")
@@ -49,16 +45,12 @@ func main() {
 		return
 	}
 
-	n1, err := file.Write([]byte(a.String()))
+	_, err = file.Write([]byte(a.String()))
 	if err != nil {
 		log.Panic(err)
 		return
 	}
 	defer file.Close()
-
-	if int64(n1) != n {
-		log.Printf("Wrote %v, Contentlength : %v", n1, n)
-	}
 
 }
 
